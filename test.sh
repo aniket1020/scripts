6f04c139364b1e418f47ce987389565f22dd4021
aniket1020 <aniket.1020@gmail.com>
2020-09-06 21:52:04 +0530

    Initial Commit --> Contains UCSD Coursera test script

---
+++ test.sh
@@ -0,0 +1,17 @@
+cnt=1;
+for fl in tests/*
+do
+	if [[ $fl == *.a ]]
+	then
+		if cmp -s $var $(cat $fl);
+		then
+			cnt=$((cnt+1));
+			continue
+		else
+			echo "[$cnt] $var --> $(cat $fl)"
+		fi
+		cnt=$((cnt+1));
+	else
+		var=$(./a.out < $fl)
+	fi;
+done
